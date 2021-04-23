69c74f2cdbd480b95b7b6beca8f2afdf39633ff6
Serhii <malgothsm@gmail.com>
2021-04-23 16:11:53 +0300

    For some reason data is undefined

--- Visualize Data with a Bar Chart/script.js
+++ Visualize Data with a Bar Chart/script.js
@@ -4,12 +4,14 @@ const svg = d3.select('svg');
 
 
 ////Global variables
-let xAxisScale;
-let yAxisScale;
-let dataValues = [];
 const width = 700;
 const height = 500;
 const padding = 50;
+let dataValues = [];
+let xAxisScale;
+let yAxisScale;
+let heighScl;
+let xScale;
 
 
 const dataChart = () => {
@@ -18,9 +20,9 @@ const dataChart = () => {
 };
 
 const createScales = () => {
-  let heighScl = d3.scaleLinear().domain([0, d3.max(dataValues, (e) => e[1])])
+  heighScl = d3.scaleLinear().domain([0, d3.max(dataValues, (e) => e[1])])
         .range([0, height - (2*padding)]);
-  let xScale = d3.scaleLinear().domain([0, dataValues.length - 1])
+  xScale = d3.scaleLinear().domain([0, dataValues.length - 1])
         .range([padding, width - padding]);
   let dates = dataValues.map(e => {
     return new Date(e[0]);
@@ -35,11 +37,33 @@ const createScales = () => {
 }
 
 const createBars = () => {
-  svg.selectAll("rect").data(dataValues).enter().append('rect')
-    .attr('class', 'bar').attr('width', (width - (2 * padding)) / values.length)
-    .attr('data-date', (e) => {
-              return e[0];
-          })
+  let tooltip = d3
+  .select("body")
+  .append("div")
+  .attr("id", "tooltip")
+  .style("opacity", 0);
+
+  svg.selectAll("rect").data(dataValues).enter().append("rect")
+    .attr("class", "bar").attr("width", (width - (2 * padding)) / dataValues.length)
+    .attr("data-date", (e) => e[0])
+    .attr("data-gdp", (e) => e[1])
+    .attr("height", (e) => {
+      return heighScl(e[1])})
+    .attr("x", (e, index) => {
+      return xScale(index)})
+    .attr("y", (e) => {
+      return (height - padding) - heighScl(e[1])})
+    .on("mouseover", (d) => {
+        tooltip.transition().style("opacity", 1)
+        tooltip.text(d[0])
+        document.querySelector("#tooltip").setAttribute("data-date", d[0])
+      })
+    .on("mouseout", (d) => {
+        tooltip.transition().style("opacity", 0)
+        // tooltip.removeAttribute("data-date", e[1]) 
+      })
+      ////Color of Graphic
+    .attr("cx",150).attr("cy",100).attr("r",20).style("fill", d3.color("yellow"));  
 }
 
 const createAxis = () => {
