f056f13d36e45b1bd7e91f9ec9050ff2a8aab8bf
Serhii <malgothsm@gmail.com>
2021-04-23 17:20:41 +0300

    Redone and finished the project

--- Visualize Data with a Bar Chart/script.js
+++ Visualize Data with a Bar Chart/script.js
@@ -1,91 +1,79 @@
-const request = new XMLHttpRequest();
-const dataset = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
-const svg = d3.select('svg');
+fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json').then(e => e.json()).then(e => {
+    const {data} = e;
+  workOnData(data.map(d => [d[0], d[1], d[0].split(" ")[0]])); 
+});
 
 
-////Global variables
-const width = 700;
+////Global Variables
 const height = 500;
-const padding = 50;
-let dataValues = [];
-let xAxisScale;
-let yAxisScale;
-let heighScl;
-let xScale;
+const width = 700;
+const padding = 40;
+const svg = d3.select('svg').attr('width', width).attr('height', height);
 
+function workOnData(data){
 
-const dataChart = () => {
-  svg.attr("width", width);
-  svg.attr("height", height);
-};
+  ////Placement of the Chart
+  const yScale = d3.scaleLinear()
+                .domain([0, d3.max(data, d => d[1])])
+                .range([height, padding * 2]);
+  
+  const xScale = d3.scaleTime()
+                  .domain([d3.min(data, d => new Date(d[0])), d3.max(data, d => new Date(d[0]))])
+                  .range([padding, width - padding]);
+  
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale);
 
-const createScales = () => {
-  heighScl = d3.scaleLinear().domain([0, d3.max(dataValues, (e) => e[1])])
-        .range([0, height - (2*padding)]);
-  xScale = d3.scaleLinear().domain([0, dataValues.length - 1])
-        .range([padding, width - padding]);
-  let dates = dataValues.map(e => {
-    return new Date(e[0]);
-  });
-  console.log(dates);
-   xAxisScale = d3.scaleTime().domain([d3.min(dates), d3.max(dates)])
-                    .range([padding, width-padding])
+  const widthOfBar = (width - 2 * padding) / data.length;
+    const tooltip = d3.select('body')
+    .append('div')
+    .attr('id', 'tooltip')
+    .style('opacity', 0);  
 
-  yAxisScale = d3.scaleLinear().domain([0, d3.max(dataValues, (e) => {
-                        return e[1]
-                    })]).range([height - padding, padding])
+  ////SVG Stuff
+  svg.append('g')
+  .attr('id', 'x-axis')
+  .attr('transform', `translate(0, ${height - padding})`)
+  .call(xAxis);
+
+  svg.append('g')
+  .attr('id', 'y-axis')
+  .attr('transform', `translate(${padding}, ${-padding})`)
+  .call(yAxis);
+
+  svg.append('text')
+  .attr('transform', 'rotate(-90)')
+  .attr('x', -250)
+  .attr('y', 80);
+
+   ////Exc. goals
+    svg.selectAll('rect')
+        .data(data)
+        .enter()
+        .append('rect')
+        .attr('x', (d, i) => i * widthOfBar + padding) 
+        .attr('y', d => yScale(d[1]) - padding)
+        .attr('width', widthOfBar)
+        .attr('height', d => height - yScale(d[1]))
+        .attr('class', 'bar')
+        .attr('data-date', d => d[0])
+        .attr('data-gdp', d => d[1])
+        .on('mouseover', (d, i) => {
+        var timeFormat = new Intl.NumberFormat();
+     
+      tooltip.attr('data-date', i[0])
+               .html(`<p>"${i[0]}"<br><b>$${timeFormat.format(i[1])} Billions Dollars</p>`)
+               .transition()
+               .duration(100)
+               .style('opacity', '1')
+               .style("display", "block");
+      
+        }).on('mouseout', d => {
+           tooltip.transition()
+                .duration(100)
+                .style('opacity', '0')
+                .style('display', 'none');
+       })
+  
+   
 }
-
-const createBars = () => {
-  let tooltip = d3
-  .select("body")
-  .append("div")
-  .attr("id", "tooltip")
-  .style("opacity", 0);
-
-  svg.selectAll("rect").data(dataValues).enter().append("rect")
-    .attr("class", "bar").attr("width", (width - (2 * padding)) / dataValues.length)
-    .attr("data-date", (e) => e[0])
-    .attr("data-gdp", (e) => e[1])
-    .attr("height", (e) => {
-      return heighScl(e[1])})
-    .attr("x", (e, index) => {
-      return xScale(index)})
-    .attr("y", (e) => {
-      return (height - padding) - heighScl(e[1])})
-    .on("mouseover", (d) => {
-        tooltip.transition().style("opacity", 1)
-        tooltip.text(d[0])
-        document.querySelector("#tooltip").setAttribute("data-date", d[0])
-      })
-    .on("mouseout", (d) => {
-        tooltip.transition().style("opacity", 0)
-        // tooltip.removeAttribute("data-date", e[1]) 
-      })
-      ////Color of Graphic
-    .attr("cx",150).attr("cy",100).attr("r",20).style("fill", d3.color("yellow"));  
-}
-
-const createAxis = () => {
-  let xAxis = d3.axisBottom(xAxisScale);
-  svg.append('g').call(xAxis)
-    .attr("id","x-axis").attr("color", "white")
-    .attr("transform", "translate(0, " + (height-padding) + ")")
-  let yAxis = d3.axisLeft(yAxisScale);
-  svg.append('g').call(yAxis)
-    .attr("id", "y-axis").attr("color", "white")
-    .attr("transform", "translate(" + (padding)+ ", 0)")
-}
-
-
-request.open("GET", dataset, true);
-request.onload = () => {
- let data = JSON.parse(request.responseText);
- dataValues = data.data;
- console.log(dataValues);
- dataChart();
- createScales();
- createBars();
- createAxis();
-}
-request.send()
