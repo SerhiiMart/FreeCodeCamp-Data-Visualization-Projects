286225595336e9fbdd13b75e2a9d22c349fa1388
Serhii <malgothsm@gmail.com>
2021-04-26 15:05:53 +0300

    Finished the third

--- Visualize Data with a Heat Map/script.js
+++ Visualize Data with a Heat Map/script.js
@@ -1,58 +1,75 @@
 ////Global Variables
 const width = 700;
 const height = 500;
-const padding = 30;
-
-const tooltip = document.getElementById('tooltip');
-const svg = d3.select('.main').append('svg')
-.attr('width', width)
-.attr('height', height);
-let basicTemperature = undefined;
-
+const padding = 60;
+let tooltip = document.getElementById('tooltip');
+const svg = d3.select('.main').append('svg').attr('width', width).attr('height', height);
+const colors = ["#67001f","#b2182b","#d6604d","#f4a582","#fddbc7","#f7f7f7","#d1e5f0","#92c5de","#4393c3","#2166ac","#053061"];
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
 fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json').then(e => e.json()).then(e => {
   const { baseTemperature, monthlyVariance } = e; 
-  workOnData(monthlyVariance);
+  workOnData(monthlyVariance.map(e => ({...e, temp: baseTemperature - e.variance})));
 })
 
 function workOnData(data) {
-  const barWidth = width / data.length; 
+  const graphHeight = (height - (2*padding)) / 12;
+  const graphWidth = width / Math.floor(data.length / 12);
+  
   const yScale = d3.scaleLinear()
-      .domain([0, d3.max(data, d => d[1])])
-      .range([height - padding, padding]);
+      .domain([0, 11])
+      .range([padding, height - padding]);
   
   const xScale = d3.scaleTime()
-      .domain([d3.min(data, d => new Date(d[0])), d3.max(data, d => new Date(d[0]))])
-      .range([padding, width - padding])
-      
+      .domain([
+        d3.min(data, d => d.year), 
+        d3.max(data, d => d.year)
+       ])
+      .range([padding, width - padding]);
+
+  const tempScale = d3.scaleLinear()
+    .domain([
+      d3.min(data, d => d.temp), 
+      d3.max(data, d => d.temp)
+    ])
+    .range([0, 10]);
+
   svg.selectAll('rect')
     .data(data)
     .enter()
     .append('rect')
-    .attr('class', 'bar')
-    .attr('data-date', d => d[0])
-    .attr('data-gdp', d => d[1])
-    .attr('x', d => xScale(new Date(d[0])))
-    .attr('y', d => yScale(d[1]))
-    .attr('width', barWidth)
-    .attr('height', d => height - yScale(d[1]) - padding)
+    .attr('class', 'cell')
+    .attr('data-month', d => d.month - 1)
+    .attr('data-year', d => d.year)
+    .attr('data-temp', d => d.temp)
+    .attr('fill', d => colors[Math.floor(tempScale(d.temp))])
+    .attr('x', d => xScale(d.year))
+    .attr('y', d => yScale(d.month - 1) - graphHeight)
+    .attr('width', graphWidth)
+    .attr('height', graphHeight)
     .on('mouseover', (d, i) => {
-    
       tooltip.classList.add('show');
-      tooltip.style.left = i * barWidth + padding * 2 + 'px';
-      tooltip.style.top = height - padding * 4 + 'px';
-      tooltip.setAttribute('data-date', d[0])
+      tooltip.style.left = xScale(d.year) - 60 + 'px';
+      tooltip.style.top = yScale(d.month - 1) - 60 + 'px';
+      tooltip.setAttribute('data-year', d.year);
 
       tooltip.innerHTML = `
-        <small>${d[0]}</small>
-        $${d[1]} billions
+        <p>${d.year} - ${months[d.month - 1]}</p>
+        &nbsp;&nbsp;&nbsp;
+        <p>${d.temp}℃</p>
       `;
   }).on('mouseout', () => {
      tooltip.classList.remove('show');
   });
   
   const xAxis = d3.axisBottom(xScale)
+    .tickFormat(d3.format('d'));
   const yAxis = d3.axisLeft(yScale)
+    .tickFormat((month) => {
+    const date = new Date(0);
+    date.setUTCMonth(month);
+    return d3.timeFormat('%B')(date);
+  });
   
   svg.append('g')
     .attr('id', 'x-axis')
@@ -61,6 +78,25 @@ function workOnData(data) {
   
   svg.append('g')
     .attr('id', 'y-axis')
-    .attr('transform', `translate(${padding}, 0)`)
-    .call(yAxis)
+    .attr('transform', `translate(${padding}, ${-graphHeight})`)
+    .call(yAxis);
+
+  const legendWidth = 500;
+  const legendHeight = 50;
+  
+  const legendRectWidth = legendWidth / colors.length;
+  const legend = d3.select('body')
+    .append('svg')
+    .attr('id', 'legend')
+    .attr('width', legendWidth)
+    .attr('height', legendHeight)
+    .selectAll('rect')
+    .data(colors)
+    .enter()
+    .append('rect')
+    .attr('x', (_, i) => i * legendRectWidth)
+    .attr('y', 0)
+    .attr('width', legendRectWidth)
+    .attr('height', legendHeight)
+    .attr('fill', c => c)
 }
