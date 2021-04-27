d168e60b8f1a1771dfd11381f33cdbe82cfea75d
Serhii <malgothsm@gmail.com>
2021-04-27 16:53:55 +0300

    Choropleth Map doesn't working. Trying to fix it

--- Visualize Data with a Choropleth Map/script.js
+++ Visualize Data with a Choropleth Map/script.js
@@ -1,97 +1,71 @@
-////Global Variables
-let tooltip = document.getElementById('tooltip');
-const svg = d3.select('.main').append('svg').attr('width', width).attr('height', height);
-const width = 700;
-const height = 500;
-const padding = 60;
+////Globals
+const colors = [];
+const width = 1000;
+const height = 700;
+const tooltip = document.getElementById('tooltip');
 
-const colors = ["#67001f","#b2182b","#d6604d","#f4a582","#fddbc7","#f7f7f7","#d1e5f0","#92c5de","#4393c3","#2166ac","#053061"];
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
-fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json').then(e => e.json()).then(e => {
-  const { baseTemperature, monthlyVariance } = e; 
-  workOnData(monthlyVariance.map(e => ({...e, temp: baseTemperature - e.variance})));
-})
-
-function workOnData(data) {
-  const graphHeight = (height - (2*padding)) / 12;
-  const graphWidth = width / Math.floor(data.length / 12);
+(async function edMap() {
+  const eduResp = await fetch('https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json');
+  const countiesResp = await fetch('https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json');
+  const educations = await eduResp.json();
+  const counties = await countiesResp.json();
+  const path = d3.geoPath();
+  const data = topojson.feature(counties, counties.objects.counties).features;
+  const minEdu = d3.min(educations, edu => edu.bachelorsOrHigher);
+  const maxEdu = d3.max(educations, edu => edu.bachelorsOrHigher);
+  const step = (maxEdu - minEdu) / 8;
+  const colorsScale = d3.scaleThreshold()
+    .domain(d3.range(minEdu, maxEdu, step))
+    .range(d3.schemePurples[9]);
+  for(let i=minEdu; i<=maxEdu; i+=step) {
+    colors.push(i);
+  }
   
-  const yScale = d3.scaleLinear()
-      .domain([0, 11])
-      .range([padding, height - padding]);
+  const svg = d3.select('.main').append('svg')
+    .attr('width', width)
+    .attr('height', height);
   
-  const xScale = d3.scaleTime()
-      .domain([
-        d3.min(data, d => d.year), 
-        d3.max(data, d => d.year)
-       ])
-      .range([padding, width - padding]);
-
-  const tempScale = d3.scaleLinear()
-    .domain([
-      d3.min(data, d => d.temp), 
-      d3.max(data, d => d.temp)
-    ])
-    .range([0, 10]);
-
-  svg.selectAll('rect')
+  svg.append('g')
+    .selectAll('path')
     .data(data)
     .enter()
-    .append('rect')
-    .attr('class', 'cell')
-    .attr('data-month', d => d.month - 1)
-    .attr('data-year', d => d.year)
-    .attr('data-temp', d => d.temp)
-    .attr('fill', d => colors[Math.floor(tempScale(d.temp))])
-    .attr('x', d => xScale(d.year))
-    .attr('y', d => yScale(d.month - 1) - graphHeight)
-    .attr('width', graphWidth)
-    .attr('height', graphHeight)
+    .append('path')
+    .attr('class', 'county')
+    .attr('fill', d => colorsScale(educations.find(edu => edu.fips === d.id).bachelorsOrHigher))
+    .attr('d', path)
+    .attr('data-fips', d => d.id)
+    .attr('data-education', d => educations.find(edu => edu.fips === d.id).bachelorsOrHigher)
     .on('mouseover', (d, i) => {
+      const { coordinates } = d.geometry;
+      const [x, y] = coordinates[0][0];
+
+      const education = educations.find(edu => edu.fips === d.id);
+
       tooltip.classList.add('show');
-      tooltip.style.left = xScale(d.year) - 60 + 'px';
-      tooltip.style.top = yScale(d.month - 1) - 60 + 'px';
-      tooltip.setAttribute('data-year', d.year);
+      tooltip.style.left = x - 50 + 'px';
+      tooltip.style.top = y - 50 + 'px';
+      tooltip.setAttribute('data-education', education.bachelorsOrHigher);
 
       tooltip.innerHTML = `
-        <p>${d.year} - ${months[d.month - 1]}</p>
-        &nbsp;&nbsp;&nbsp;
-        <p>${d.temp}℃</p>
+        <p>${education.area_name} - ${education.state}</p>
+        <p>${education.bachelorsOrHigher}%</p>
       `;
   }).on('mouseout', () => {
-     tooltip.classList.remove('show');
+    tooltip.classList.remove('show');
   });
-  
-  const xAxis = d3.axisBottom(xScale)
-    .tickFormat(d3.format('d'));
-  const yAxis = d3.axisLeft(yScale)
-    .tickFormat((month) => {
-    const date = new Date(0);
-    date.setUTCMonth(month);
-    return d3.timeFormat('%B')(date);
-  });
-  
-  svg.append('g')
-    .attr('id', 'x-axis')
-    .attr('transform', `translate(0, ${height - padding})`)
-    .call(xAxis);
-  
-  svg.append('g')
-    .attr('id', 'y-axis')
-    .attr('transform', `translate(${padding}, ${-graphHeight})`)
-    .call(yAxis);
+  const legendWidth = 200;
+  const legendHeight = 30;
 
-  const legendWidth = 500;
-  const legendHeight = 50;
-  
   const legendRectWidth = legendWidth / colors.length;
-  const legend = d3.select('body')
+  const legend = d3.select('.main')
     .append('svg')
     .attr('id', 'legend')
+    .attr('class', 'legend')
     .attr('width', legendWidth)
     .attr('height', legendHeight)
-    .selectAll('rect')
+   
+  legend.selectAll('rect')
     .data(colors)
     .enter()
     .append('rect')
@@ -99,5 +73,5 @@ function workOnData(data) {
     .attr('y', 0)
     .attr('width', legendRectWidth)
     .attr('height', legendHeight)
-    .attr('fill', c => c)
-}
+    .attr('fill', c => colorsScale(c))
+})();
