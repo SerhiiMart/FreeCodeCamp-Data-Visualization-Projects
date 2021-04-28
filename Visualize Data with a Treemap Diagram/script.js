040fe8482bc46ff4ec646885653e554861ab6e35
Serhii <malgothsm@gmail.com>
2021-04-28 15:23:46 +0300

    Adapting the function

--- Visualize Data with a Treemap Diagram/script.js
+++ Visualize Data with a Treemap Diagram/script.js
@@ -1,73 +1,89 @@
 ////Globals
 const width = 1000;
 const height = 700;
+
 const tooltip = document.getElementById('tooltip');
 const svg = d3.select('.main').append('svg')
 .attr('width', width)
 .attr('height', height);
-const colors = [];
+const treeMap = d3.treemap().size([width, height]).padding(2);
+const colors = d3.scaleOrdinal(d3.schemePaired);
 
-(async function edMap() {
-  const eduResp = await fetch('https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json');
-  const countiesResp = await fetch('https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json');
-  const educat = await eduResp.json();
-  const counties = await countiesResp.json();
-  const path = d3.geoPath();
-  const data = topojson.feature(counties, counties.objects.counties).features;
-  const minEdu = d3.min(educat, edu => edu.bachelorsOrHigher);
-  const maxEdu = d3.max(educat, edu => edu.bachelorsOrHigher);
-  const step = (maxEdu - minEdu) / 8;
-  const colorsScale = d3.scaleThreshold().domain(d3.range(minEdu, maxEdu, step))
-  .range(d3.schemeYlGnBu[9]);
-  for(let i=minEdu; i<=maxEdu; i+=step) {
-    colors.push(i);
-  }
+(async function movies() {
+  const movieSales = await fetch('https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json').then(e => e.json());
   
-  svg.append('g')
-    .selectAll('path')
-    .data(data)
+  const root = d3.hierarchy(movieSales).sum(d => d.value);
+
+  treeMap(root);
+
+const cell = svg.selectAll('g')
+  .data(root.leaves())
+  .enter().append('g')
+  .attr('transform', d => `translate(${d.x0}, ${d.y0})`);
+
+const tile = cell.append('rect')
+  .attr('class', 'tile')
+  .attr('data-name', d => d.data.name)
+  .attr('data-category', d => d.data.category)
+  .attr('data-value', d => d.data.value)
+  .attr('width', d => d.x1 - d.x0)
+  .attr('height', d => d.y1 - d.y0)
+  .attr('fill', d => color(d.data.category))
+  .on('mouseover', (d, i) => {
+    const { name, category, value } = d.data;
+    tooltip.classList.add('show');
+    tooltip.style.left = (d3.event.pageX) + 'px';
+    tooltip.style.top = (d3.event.pageY - 100) + 'px';
+    tooltip.setAttribute('data-value', value);
+
+    tooltip.innerHTML = `
+      <p><strong>Name:</strong> ${name}</p>
+      <p><strong>Category:</strong> ${category}</p>
+      <p><strong>Value:</strong> ${value}</p>
+    `;
+}).on('mouseout', () => {
+  tooltip.classList.remove('show');
+});
+
+cell.append('text')
+  .selectAll('tspan')
+  .data(d => d.data.name.split(/(?=[A-Z][^A-Z])/g))
+  .enter().append('tspan')
+  .attr('style', 'font-size: 13px')
+  .attr('x', 4)
+  .attr('y', (d, i) => 15 + i * 15)
+  .text(d => d)
+
+const categories = root.leaves().map(n => n.data.category).filter((item, idx, arr) => arr.indexOf(item) === idx);
+
+const blockSize = 20;
+const legendWidth = 200;
+const legendHeight = (blockSize + 2) * categories.length;
+
+const legend = d3.select('body')
+  .append('svg')
+  .attr('id', 'legend')
+  .attr('width', legendWidth)
+  .attr('height', legendHeight)
+ 
+legend.selectAll('rect')
+  .data(categories)
+  .enter()
+  .append('rect')
+  .attr('class', 'legend-item')
+  .attr('fill', d => color(d))
+  .attr('x', blockSize / 2)
+  .attr('y', (_, i) => i * (blockSize + 1) + 10)
+  .attr('width', blockSize)
+  .attr('height', blockSize)
+ 
+ legend.append('g')
+    .selectAll('text')
+    .data(categories)
     .enter()
-    .append('path')
-    .attr('class', 'county')
-    .attr('fill', d => colorsScale(educat.find(edu => edu.fips === d.id).bachelorsOrHigher))
-    .attr('d', path)
-    .attr('data-fips', d => d.id)
-    .attr('data-education', d => educat.find(edu => edu.fips === d.id).bachelorsOrHigher)
-    .on('mouseover', (d, i) => {
-      const { coordinates } = d.geometry;
-      const [x, y] = coordinates[0][0];
-      const education = educat.find(edu => edu.fips === d.id);
-
-      tooltip.classList.add('show');
-      tooltip.style.left = x - 50 + 'px';
-      tooltip.style.top = y - 50 + 'px';
-      tooltip.setAttribute('data-education', education.bachelorsOrHigher);
-
-      tooltip.innerHTML = `
-        <p>${education.area_name} - ${education.state}</p>
-        <p>${education.bachelorsOrHigher}%</p>
-      `;
-  }).on('mouseout', () => {
-    tooltip.classList.remove('show');
-  });
-  const legendWidth = 200;
-  const legendHeight = 30;
-
-  const legendRectWidth = legendWidth / colors.length;
-  const legend = d3.select('.main')
-    .append('svg')
-    .attr('id', 'legend')
-    .attr('class', 'legend')
-    .attr('width', legendWidth)
-    .attr('height', legendHeight)
-   
-  legend.selectAll('rect')
-    .data(colors)
-    .enter()
-    .append('rect')
-    .attr('x', (_, i) => i * legendRectWidth)
-    .attr('y', 0)
-    .attr('width', legendRectWidth)
-    .attr('height', legendHeight)
-    .attr('fill', c => colorsScale(c))
+    .append('text')
+    .attr('fill', 'black')
+    .attr('x', blockSize * 2)
+    .attr('y', (_, i) => i * (blockSize + 1) + 25)
+    .text(d => d)
 })();
